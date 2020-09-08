eb282d5a10761a34b3e7bc9be0aaac3b0e8097d3
Noah Camargo <noah.camargo@hotmail.com>
2020-09-08 01:07:36 -0300

    Add rack-cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+  before_action :allow_ajax_request_from_other_domains
+
+  def options
+    head(:no_content)
+  end
+
+  protected
+
+  def allow_ajax_request_from_other_domains
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,13 @@ module AccountingChallenge
     config.i18n.default_locale = :'pt-BR'
     config.i18n.locale = :'pt-BR'
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: %i[get post put delete options head]
+      end
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
